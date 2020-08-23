aa3cf54b2c14775cd165ab5b0404c807fef85d70
Van Euclid <68357572+VanEuclid@users.noreply.github.com>
2020-08-22 20:44:39 -0600

    First commit
    DB created

---
+++ ConserveDB/ConserveDB/Models/Member.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ConserveDB.Models
+{
+    public class Member
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string EmailAddress { get; set; }
+        public string PreferredContactPhoneNumber { get; set; }
+        public string Position { get; set; }
+        public string Department { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime StartDate { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime EndDate { get; set; }
+
+        public string EmploymentStatus { get; set; }
+        public string Shift { get; set; }
+        public string Manager { get; set; }
+        public string TeamMemberPhoto { get; set; }
+        public string FavoriteColor { get; set; }
+
+    }
+}
