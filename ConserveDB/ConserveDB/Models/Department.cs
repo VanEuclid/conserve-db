5c3dd45cf798625718efabb06405dc1362900f2c
Van Euclid <68357572+VanEuclid@users.noreply.github.com>
2020-09-01 22:39:40 -0600

    migration completed

--- ConserveDB/ConserveDB/Models/Department.cs
+++ ConserveDB/ConserveDB/Models/Department.cs
@@ -6,7 +6,13 @@ namespace ConserveDB.Models
     public class Department
     {
         public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Department Name")]
         public string departmentName { get; set; }
+
+        [Required]
+        [Display(Name = "Position")]
         public string position { get; set; }
     }
 }
