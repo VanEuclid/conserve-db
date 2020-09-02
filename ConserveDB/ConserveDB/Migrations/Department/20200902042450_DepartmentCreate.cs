65b9efaaf5e632099374ce538fdcf5c6a760ecbd
Van Euclid <68357572+VanEuclid@users.noreply.github.com>
2020-09-01 22:26:36 -0600

    Department Database made

---
+++ ConserveDB/ConserveDB/Migrations/Department/20200902042450_DepartmentCreate.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ConserveDB.Migrations.Department
+{
+    public partial class DepartmentCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Department",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    departmentName = table.Column<string>(nullable: true),
+                    position = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Department", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Department");
+        }
+    }
+}
